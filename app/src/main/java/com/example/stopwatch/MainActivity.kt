dad21d4566578d26a5c1e88ffd065cece0315d79
notaplayer4768 <thad.philpot@gmail.com>
2022-09-08 10:07:33 -0700

    Completed Layout

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    //how to make classwide static constant in kotlin
+    companion object {
+        //all your "static" constants go here
+        val TAG = "MainActivity"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: ")
     }
 }

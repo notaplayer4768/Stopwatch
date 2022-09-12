73208f79ba86e2e9c3597453cd55174d8be9cc5b
notaplayer4768 <thad.philpot@gmail.com>
2022-09-12 08:56:14 -0700

    Completed Layout

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -17,26 +17,26 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        Log.d(TAG, "onStart: ")
+        Log.v(TAG, "onStart: ")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.v(TAG,"onResume")
+        Log.v(TAG,"onResume: ")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.v(TAG,"onPause")
+        Log.v(TAG,"onPause: ")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.v(TAG,"onStop")
+        Log.v(TAG,"onStop: ")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.v(TAG,"onDestroy")
+        Log.v(TAG,"onDestroy: ")
     }
 }

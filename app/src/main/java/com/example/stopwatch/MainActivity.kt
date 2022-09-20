5712cf17666115d6f194b5d373d792fc0e7cc9e7
notaplayer4768 <thad.philpot@gmail.com>
2022-09-20 10:06:13 -0700

    Completed Layout

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.os.SystemClock
 import android.util.Log
 import android.view.View
@@ -14,6 +15,9 @@ class MainActivity : AppCompatActivity() {
     companion object {
         //all your "static" constants go here
         val TAG = "MainActivity"
+        val STATE_TIME = "display time"
+        val IS_RUNNING = "if is running"
+
     }
     private lateinit var start : Button
     private lateinit var reset : Button
@@ -21,12 +25,22 @@ class MainActivity : AppCompatActivity() {
     private lateinit var debug : TextView
     private var base = 0L
     private var saysStart = true
+    private var stopTime = 0L
+    private var displayTime = 0L
+    private var stopIndex = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         wideWidgets()
-
+        //restore instance state if it exists
+        if(savedInstanceState!= null) {
+            displayTime = savedInstanceState.getLong(STATE_TIME)
+            timer.base = SystemClock.elapsedRealtime()-displayTime
+            saysStart = savedInstanceState.getBoolean(IS_RUNNING)
+            if(!saysStart)
+                timer.start()
+        }
         start.setOnClickListener{
             debug.setText("${SystemClock.elapsedRealtime()} base $base")
             if(!saysStart)
@@ -34,19 +48,34 @@ class MainActivity : AppCompatActivity() {
                 saysStart = true
                 start.setText("Start")
                 timer.stop()
-                base = SystemClock.elapsedRealtime()
+                stopIndex = SystemClock.elapsedRealtime()
             }
             else if(saysStart)
             {
                 saysStart = false
                 start.setText("Stop")
-                timer.base = base
+                stopTime += SystemClock.elapsedRealtime() - stopIndex
+                timer.base = SystemClock.elapsedRealtime() - stopTime
                 timer.start()
-
             }
+        }
+        reset.setOnClickListener{
+            //placeholder
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        //preserve state through orientation changes
+        //save key value pairs to the bundle before the superclass call
+        //calculate the display time
+        if(!saysStart) {
+            displayTime = SystemClock.elapsedRealtime() - timer.base
+        }
+        outState.putBoolean(IS_RUNNING,saysStart)
+
+        outState.putLong(STATE_TIME,displayTime)
+        super.onSaveInstanceState(outState)
+    }
     private fun wideWidgets() {
         start = findViewById(R.id.button_main_start)
         reset = findViewById(R.id.button_main_reset)
